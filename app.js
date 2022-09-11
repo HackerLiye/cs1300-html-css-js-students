d0719ba2e371af94e5b35631fad710f8782e596e
Youcheng Liye <liye@youchengdembp.devices.brown.edu>
2022-09-11 17:31:11 -0400

    initial commit

--- app.js
+++ app.js
@@ -26,25 +26,38 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
+  var fruits=fruitsArray.filter(fruit=>{
+    
+    console.log(fruit.name)
+    if (fruit.name.length>6){
+      // console.log(fruit.name)
+      return true;
+    }
+  })
 
   // TODO: Create a new HTML element to display your data
-
+  
   // TODO: Append your new element to the page
+  fruits.forEach(function(item, index, array) {
+    console.log(item.name)
+    exampleAddElement(item.name)
+  })
+  
 
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
-const exampleAddElement = () => {
+const exampleAddElement = (fruit) => {
   // Create a new HTML element and set its properties
   const newElement = document.createElement('div');
-  newElement.innerHTML = "this text is inside a div";
+  newElement.innerHTML = fruit;
 
   // Append the new element to an existing part of the webpage
-  const existingElement = document.getElementById('example-id');
+  const existingElement = document.getElementById('cs1300-gallery');
   existingElement.append(newElement);
 }
 
